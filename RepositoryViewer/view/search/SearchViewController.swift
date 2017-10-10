36ef2cee480a00243fd4ab3f54b4f2d677572499
tsk takezawa <tsk.take815@gmail.com>
2017-10-10 13:11:10 +0900

    Layout tab view

---
+++ RepositoryViewer/view/search/SearchViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+final class SearchViewController: UIViewController {
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
